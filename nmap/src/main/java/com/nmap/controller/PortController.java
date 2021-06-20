f60fe8ce26c6ef8d84d5c754635ef997039b2689
Jeevan Abraham <jabraham@inmoment.com>
2021-06-20 17:41:04 -0600

    add cross orgin support

--- nmap/src/main/java/com/nmap/controller/PortController.java
+++ nmap/src/main/java/com/nmap/controller/PortController.java
@@ -1,6 +1,7 @@
 package com.nmap.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class PortController {
 	@Autowired
 	private PortService postService; 
 	
+	@CrossOrigin
 	@GetMapping("/open-port/{hostName}")
 	public PortInformation getOpenPort(@PathVariable String hostName) {
 		return postService.getOpenPortsByHostName(hostName);
